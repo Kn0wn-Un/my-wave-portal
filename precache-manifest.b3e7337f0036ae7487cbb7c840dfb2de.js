a79ad223bd0f0a439e232b0ba311eb60f21fe5b7
Darshan H M <naikdarsh175@gmail.com>
2021-10-13 12:53:21 +0530

    Updates

---
+++ precache-manifest.b3e7337f0036ae7487cbb7c840dfb2de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db01ba0f6ea522cf8b3e8a7e8f2b4ec3",
+    "url": "/my-wave-portal/index.html"
+  },
+  {
+    "revision": "2fb9117465924d0b37d9",
+    "url": "/my-wave-portal/static/css/main.4100b12d.chunk.css"
+  },
+  {
+    "revision": "54ea54bec270ff7a3fc8",
+    "url": "/my-wave-portal/static/js/2.bdf5d5d3.chunk.js"
+  },
+  {
+    "revision": "2fb9117465924d0b37d9",
+    "url": "/my-wave-portal/static/js/main.482d3245.chunk.js"
+  },
+  {
+    "revision": "dc7b73033a3979fd32c3",
+    "url": "/my-wave-portal/static/js/runtime-main.bd638210.js"
+  }
+]);
